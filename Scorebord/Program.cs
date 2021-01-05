e0a8f8ad7ca7ce90a3f753c005b0cb93b62cd398
MAURO GAY <MAURO.GAY@student.intecbrussel.be>
2021-01-05 08:46:45 +0100

    Add project files.

---
+++ Scorebord/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scorebord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persoon s1 = new Persoon();
+            s1.Naam = "emre";
+
+            List<Persoon> Personen = new List<Persoon>();
+            Persoon.add (s1);
+            
+            List<student> Students = new List<student>
+            {
+            new Persoon(){Id=1, Name="Toon" },
+            new Persoon(){Id=2, Name="Emre" },
+            new Persoon(){Id=3, Name="Jan" }
+            };
+
+
+            foreach (var item in Students)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+
+    }
+
+
+     class Persoon
+    {
+    public string Naam { get; set; }
+    public int Score { get; set; }
+    }
+}
